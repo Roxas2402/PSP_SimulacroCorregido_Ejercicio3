d23a02069909df736cb585db2caedbf3499a247e
roxas <jeropastrana@gmail.com>
2022-11-11 18:42:43 +0100

    a

--- src/HiloHijo.java
+++ src/HiloHijo.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class HiloHijo {
+import java.util.concurrent.Semaphore;
+
+public class HiloHijo extends Thread {
+    public static int cont;
+    public static int contTotal = 0;
+    private Semaphore semaphore;
+
+    public HiloHijo(String name, Semaphore semaphore) {
+        super(name);
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            semaphore.acquire();
+
+            cont = 0;
+            for (String color : Main.colores) {
+                if (color.equalsIgnoreCase(this.getName())) {
+                    cont++;
+                    contTotal++;
+                }
+            }
+            System.out.println("Soy " + this.getName() + " y tengo " + cont + " colores.");
+            semaphore.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
+    public static String[] colores;
+
+    static {
+        colores = new String[]{"rojo", "verde", "azul", "rojo", "verde"};
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Semaphore semaphore = new Semaphore(1);
+        HiloHijo rojo = new HiloHijo("rojo", semaphore);
+        HiloHijo azul = new HiloHijo("azul", semaphore);
+        HiloHijo verde = new HiloHijo("verde", semaphore);
+
+        rojo.start();
+        azul.start();
+        verde.start();
+
+        try {
+            rojo.join();
+            azul.join();
+            verde.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Total colores: " + HiloHijo.contTotal);
     }
 }
